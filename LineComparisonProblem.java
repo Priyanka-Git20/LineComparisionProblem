de37246a1b7d865c2bc5d766cbe99931759e04ad
priyanka <priyanka.chavan1408@gmail.com>
2022-02-06 11:59:03 +0530

    ADDED:Welcome message on the master branch.

---
+++ LineComparisonProblem.java
@@ -0,0 +1,7 @@
+package com.linecomparison;
+
+public class LineComparisonProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the line comparison computation program.");
+    }
+}
